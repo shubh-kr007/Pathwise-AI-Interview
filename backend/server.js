07bbd597e91ccba49cffeb67dd4fb693993d9ed3
Shubh <shubhkumar357@gmail.com>
2025-11-28 16:59:00 +0530

    rebuild

--- backend/server.js
+++ backend/server.js
@@ -60,11 +60,12 @@ app.use("/api/progress", progressRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-  app.get("*", (req, res) => {
+
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
+
 
 // ✅ Start server
 
